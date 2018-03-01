e9d521fea8dab5bfa005ba52e7c445d4292f327e
Eric Hegeman <miracleofmagick@gmail.com>
2018-03-01 14:52:50 -0500

    added BaseViewModel

---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using TechJobs.Models;
+
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        public List<JobFieldType> Columns { get; set; }
+
+        public String Title { get; set; }
+
+        public BaseViewModel()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in  Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
+
+    }
+}
